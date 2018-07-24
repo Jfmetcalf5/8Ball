31af6c87e319085be013ab1b06b392401add9dc1
Jfmetcalf5 <Jfmetcalf5@gmail.com>
2018-07-24 10:59:31 -0600

    Initial commit

---
+++ 8Ball/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  8Ball
+//
+//  Created by Jacob Metcalf on 7/24/18.
+//  Copyright © 2018 JfMetcalf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func flipEightBall(_ sender: UIButton) {
+        let index = Int(arc4random_uniform(UInt32(answers.count)))
+        let answer = answers[index]
+        answerLabel.text = answer
+    }
+    
+    private let answers = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no",
+        "Outlook not so good.",
+        "Very doubtful.",
+        ]
+    
+    @IBOutlet weak var answerLabel: UILabel!
+}
+
